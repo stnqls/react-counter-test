9f4e1948d88d5f084fa9dd981e63c039762b6c6e
unknown <stnqls3938@gmail.com>
2021-08-31 14:52:12 +0900

    app.js 배열로 데이터 받기

--- src/App.js
+++ src/App.js
@@ -3,14 +3,38 @@ import GlobalStyle from './styles/GlobalStyles';
 import PhoneForm from './components/PhoneForm';
 
 class App extends Component {
+  id=2
+  state = {
+    information: [
+      {
+        id:0,
+        name: '김민준',
+        phone: '010-0000-0000'
+      },
+      {
+        id:1,
+        name: '홍길동',
+        phone: '010-0000-1111'
+      },
+    ]
+  }
   handleCreate = (data) => {
-    console.log(data);
+    const {information} = this.state;
+    this.setState({
+      information: information.concat({id:this.id++, ...data})
+    })
   }
   render(){
+    const {information} = this.state;
     return (
       <React.Fragment>
       <GlobalStyle />
-      <PhoneForm onCreate={this.handleCreate}/>
+      <div>
+      <PhoneForm 
+        onCreate={this.handleCreate}
+        />
+        {JSON.stringify(information)}
+      </div>
       </React.Fragment>
     );
   }
